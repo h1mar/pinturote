19f66781cb3f16a7a57d23e9208960990f89b9f7
Himar <himar.rc@gmail.com>
2020-06-21 17:40:24 +0100

    option to choose color

--- src/public/app.js
+++ src/public/app.js
@@ -12,15 +12,27 @@ function init() {
 	//Setting up canvas
 	const canvas = document.getElementById('drawing');
 	const context = canvas.getContext('2d');
+	let strokeColor = 'black';
 
 	//Get window height & width
-	const width = canvas.width;
-	const height = canvas.height;
+	// const width = canvas.width;
+	// const height = canvas.height;
+	// canvas.width = 400;
+	// canvas.height = 400;
 
 	// Possible way to get coordinates inside element without jquery
 	// var rect = e.target.getBoundingClientRect();
 	// var x = e.clientX - rect.left; //x position within the element.
 	// var y = e.clientY - rect.top;  //y position within the element.
+
+	//Set color from select
+	function getSelectColor() {
+		let select = document.getElementById('colorSelect');
+		select.addEventListener('change', (e) => {
+			strokeColor = document.getElementById('colorSelect').value;
+			console.log(strokeColor);
+		});
+	}
 
 	//Connecting to socket
 	const socket = io();
@@ -49,15 +61,17 @@ function init() {
 		mouse.pos.y *= canvas.height;
 
 		mouse.moving = true;
-		console.log(mouse.pos.x, mouse.pos.y);
+		// console.log(mouse.pos.x, mouse.pos.y);
 	});
 
 	//Getting data sent from server to client
 	socket.on('draw_line', (data) => {
 		//Saving data
 		const line = data.line;
-		context.fillStyle = '#FF0000';
 		context.beginPath();
+		context.strokeStyle = strokeColor;
+		context.lineJoin = 'round';
+		context.lineCap = 'round';
 		context.lineWidth = 2;
 		context.moveTo(line[0].x, line[0].y);
 		context.lineTo(line[1].x, line[1].y);
@@ -72,9 +86,10 @@ function init() {
 			socket.emit('draw_line', { line: [mouse.pos, mouse.last_pos] });
 			console.log('Sending data to server');
 			mouse.move = false;
+			getSelectColor();
 		}
 		mouse.last_pos = { x: mouse.pos.x, y: mouse.pos.y };
-		setTimeout(mainLoop, 25);
+		setTimeout(mainLoop, 10);
 	}
 	mainLoop();
 }
